0a5635570655ac1376353a2835add361de7480f3
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-20 13:29:56 -0600

    Add test to Link to Show Page

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -1,12 +1,19 @@
-require 'rails-helper'
+require 'rails_helper'
 
 RSpec.describe "Merchants Index Page" do
-
   it "shows a list of Merchant names" do
     visit "/merchants"
 
     expect(page).to have_content("Merchant Index")
     expect(page).to have_link("Schroeder-Jerde")
     expect(page).to have_link("Willms and Sons")
+  end
+
+  it "links to Merchant Show" do
+    visit "/merchants"
+
+    click_link("Schroeder-Jerde")
+
+    expect(current_path).to eq("/merchants/1")
   end
 end
